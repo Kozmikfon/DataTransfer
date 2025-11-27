c534d1848a4c229912ea242147866bd0b7d01dad
Mahmut Baglama <mahmutbaglama98@gmail.com>
2025-11-27 10:50:06 +0300

    GrdDonusum ekranı tasarlanmaya basladi.

--- DonusumEkrani.Designer.cs
+++ DonusumEkrani.Designer.cs
@@ -30,11 +30,15 @@
         {
             BtnDonusumKaydet = new Button();
             BtnDonusumIptal = new Button();
+            lblBilgi = new Label();
+            BtnEkle = new Button();
+            GrdDonusum = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)GrdDonusum).BeginInit();
             SuspendLayout();
             // 
             // BtnDonusumKaydet
             // 
-            BtnDonusumKaydet.Location = new Point(265, 485);
+            BtnDonusumKaydet.Location = new Point(401, 569);
             BtnDonusumKaydet.Name = "BtnDonusumKaydet";
             BtnDonusumKaydet.Size = new Size(119, 51);
             BtnDonusumKaydet.TabIndex = 0;
@@ -43,28 +47,64 @@
             // 
             // BtnDonusumIptal
             // 
-            BtnDonusumIptal.Location = new Point(424, 485);
+            BtnDonusumIptal.Location = new Point(560, 569);
             BtnDonusumIptal.Name = "BtnDonusumIptal";
             BtnDonusumIptal.Size = new Size(119, 51);
             BtnDonusumIptal.TabIndex = 1;
             BtnDonusumIptal.Text = "İptal";
             BtnDonusumIptal.UseVisualStyleBackColor = true;
             // 
+            // lblBilgi
+            // 
+            lblBilgi.AutoSize = true;
+            lblBilgi.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            lblBilgi.Location = new Point(95, 31);
+            lblBilgi.Name = "lblBilgi";
+            lblBilgi.Size = new Size(50, 20);
+            lblBilgi.TabIndex = 2;
+            lblBilgi.Text = "label1";
+            // 
+            // BtnEkle
+            // 
+            BtnEkle.Location = new Point(252, 569);
+            BtnEkle.Name = "BtnEkle";
+            BtnEkle.Size = new Size(119, 51);
+            BtnEkle.TabIndex = 3;
+            BtnEkle.Text = "Ekle";
+            BtnEkle.UseVisualStyleBackColor = true;
+            // 
+            // GrdDonusum
+            // 
+            GrdDonusum.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            GrdDonusum.Location = new Point(95, 83);
+            GrdDonusum.Name = "GrdDonusum";
+            GrdDonusum.Size = new Size(799, 450);
+            GrdDonusum.TabIndex = 4;
+            // 
             // DonusumEkrani
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(900, 576);
+            ClientSize = new Size(964, 703);
+            Controls.Add(GrdDonusum);
+            Controls.Add(BtnEkle);
+            Controls.Add(lblBilgi);
             Controls.Add(BtnDonusumIptal);
             Controls.Add(BtnDonusumKaydet);
             Name = "DonusumEkrani";
             Text = "Veri Dönüşüm Ekranı";
+            Load += DonusumEkrani_Load;
+            ((System.ComponentModel.ISupportInitialize)GrdDonusum).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button BtnDonusumKaydet;
         private Button BtnDonusumIptal;
+        private Label lblBilgi;
+        private Button BtnEkle;
+        private DataGridView GrdDonusum;
     }
 }
--- DonusumEkrani.cs
+++ DonusumEkrani.cs
@@ -16,5 +16,53 @@ namespace DataTransfer
         {
             InitializeComponent();
         }
+
+        private void GridBaslat()
+        {
+            GrdDonusum.AutoGenerateColumns = false;
+            GrdDonusum.Columns.Clear();
+
+            var kaynakDeger= new DataGridViewTextBoxColumn
+            {
+                HeaderText = "Kaynak Değer",
+                DataPropertyName = "KaynakDeger",
+                ReadOnly=true,
+                Width = 150
+            };
+            var hedefAtanacakDeger = new DataGridViewTextBoxColumn
+            {
+                HeaderText = "Hedef Atanacak Değer",
+                DataPropertyName = "HedefAtanacakDeger",
+                ReadOnly=true,
+                Width = 150
+            };
+            var durum=new DataGridViewTextBoxColumn
+            {
+                HeaderText = "Durum",
+                DataPropertyName = "Durum",
+                ReadOnly=true,
+                Width = 100
+            };
+            var islem=new DataGridViewButtonColumn
+            {
+                HeaderText = "İşlem",
+                Text = "Düzenle",
+                UseColumnTextForButtonValue = true,
+                Width = 75
+                
+            };
+
+            GrdDonusum.Columns.AddRange(new DataGridViewColumn[]
+            {
+                kaynakDeger,
+                hedefAtanacakDeger,
+                durum,
+                islem
+            });
+        }
+        private void DonusumEkrani_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
